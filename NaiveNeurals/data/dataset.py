2076f0f1950980ad92cfd167cd046ce8a0c93723
stovorov <stoworow@gmail.com>
2018-11-27 20:44:38 +0100

    Adding compression example

--- NaiveNeurals/data/dataset.py
+++ NaiveNeurals/data/dataset.py
@@ -6,24 +6,21 @@ import numpy as np
 class DataSet:
     """Represents training set"""
 
-    def __init__(self, training_inputs: List[List[Union[int, float]]], targets: List[List[Union[int, float]]],
-                 auto_normalise: bool = False) -> None:
+    def __init__(self, training_inputs: Union[List[List], np.array], targets: Union[List[List], np.array]) -> None:
         """Initialize TrainingData object
 
         :param training_inputs: List of inputs
         :param targets: targeted outputs for provided inputs
-        :param auto_normalise: set to true if target data must be automatically normalised
         """
-        self._training_inputs = training_inputs
-        self._targets = targets
-        self._normalised = auto_normalise
+        self._training_inputs = np.array(training_inputs) if isinstance(training_inputs, list) else training_inputs
+        self._targets = np.array(targets) if isinstance(targets, list) else targets
 
     @property
     def inputs(self) -> np.array:
         """Return input data as numpy array"""
-        return np.array(self._training_inputs)
+        return self._training_inputs
 
     @property
     def targets(self) -> np.array:
         """Return targets data as numpy array"""
-        return np.array(self._targets)
+        return self._targets
